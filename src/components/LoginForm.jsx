f1c9bef4f0c64722ac8a325479dc859986f08684
marian3299 <mariana.ag3299@gmail.com>
2024-09-02 22:30:07 -0600

    clase11: testing

---
+++ src/components/LoginForm.jsx
@@ -0,0 +1,12 @@
+const LoginForm = ({ handleClick }) => {
+  return (
+    <div>
+      <h1>Debe ingresar</h1>
+      <input type="email" role="email" />
+      <input type="password" data-testid="password" />
+      <button onClick={handleClick}>Enviar</button>
+    </div>
+  );
+};
+
+export default LoginForm;
---
+++ src/test/LoginForm.test.jsx
@@ -0,0 +1,34 @@
+import { fireEvent, render, screen } from "@testing-library/react";
+import { describe, expect, test, vi } from "vitest";
+import LoginForm from "../components/LoginForm";
+
+describe("Testing LoginForm Componente", () => {
+  test("Should render title", () => {
+    render(<LoginForm />);
+    const title = screen.getByText(/debe ingresar/i); //Obtenemos el h4
+    expect(title).toBeDefined(); //Verificamos si se renderizo
+  });
+
+  test("Should render first input", () => {
+    render(<LoginForm />);
+    const email = screen.getByRole("email"); //Obtenemos el input (hay que agregar el atributo role en el input)
+    expect(email).toBeVisible(); //Verificamos si esta visible
+  });
+
+  test("Should chabge second input", () => {
+    render(<LoginForm />);
+    const pass = screen.getByTestId("password"); //Obtenemos el input de password
+    fireEvent.change(pass, { target: { value: "password123" } }); //Modificamos el input
+    expect(pass.value).toBe("password123"); //Verifica que pass.value === password123
+  });
+
+  test("Should call button function", () => {
+    //Funcion que se ejecuta cuando hacemos click en el boton
+    const handleClick = vi.fn(); //Usamos vi.fn() para decirle al teste que handleClick es una funcion (mockeamos la funcion)
+
+    render(<LoginForm handleClick={handleClick} />); //Enviamos la funcion al componente
+    const button = screen.getByText("Enviar"); //Obtenemos el boton
+    fireEvent.click(button); //Hacemos click en el boton
+    expect(handleClick).toBeCalledTimes(1); //Verificamos que handleClick haya sido llamada una vez al hacer click en el boton
+  });
+});
